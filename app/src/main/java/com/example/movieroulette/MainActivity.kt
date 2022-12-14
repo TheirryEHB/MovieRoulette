9c4fc94a54aebd0951b7447ffd4cdef6408267f9
Theirry <thierry.klougbo@student.ehb.be>
2022-12-14 19:35:19 +0100

    Actionbar voor searching toegevoegd

--- app/src/main/java/com/example/movieroulette/MainActivity.kt
+++ app/src/main/java/com/example/movieroulette/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.movieroulette
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -17,18 +24,36 @@ import kotlinx.coroutines.runBlocking
 import org.json.JSONException
 import org.json.JSONObject
 import com.google.firebase.firestore.FirebaseFirestore
-
-
-
+import org.jetbrains.anko.find
 
 
 class MainActivity : AppCompatActivity() {
+
+    //actionbar of navigation drawer
+    //TODO Ondersteuning van landscape/portrait mode (m.a.w. verschillende layoutbestanden) voor minstens 1 activity
+    //TODO minimale ondersteuning van hdpi, xhdpi, xxhdpi en xxxhdpi schermen
+    //TODO Gebruik van JUnit-framework voor het schrijven van testen voor je applicatie (minimum 1)
+    //TODO Correcte toepassing van de navigation design patterns binnen Android
+    //TODO Ondersteuning voor minstens Engels en een andere taal naar keuze.
+
     private val data = ArrayList<JSONObject>()
+    private lateinit var searchBar:LinearLayout
 
     val room = RoomDBHelper()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val recyclerView: RecyclerView = findViewById(R.id.front_recycle_view)
+        val newGameButton = findViewById<Button>(R.id.new_game_button)
+        searchBar = findViewById(R.id.search_bar)
+        val searchButton: Button = findViewById(R.id.search_button)
+        val searchEditText: EditText = findViewById(R.id.search_edit_text)
+//        val parentView = findViewById<ConstraintLayout>(R.id.main_constraint)
+
+        searchBar.visibility = View.INVISIBLE
+//        val params = recyclerView.layoutParams as ConstraintLayout.LayoutParams
+//        params.topToTop = parentView.id
+//        recyclerView.requestLayout()
 
         //RoomDb
         room.db = Room.databaseBuilder(
@@ -39,22 +64,12 @@ class MainActivity : AppCompatActivity() {
         //Make top week movies url
         val topWeekUrl = getTrendingMoviesOfWeek()
         //Make recyclerView
-        val recyclerView: RecyclerView = findViewById(R.id.front_recycle_view)
         recyclerView.layoutManager = GridLayoutManager(this, 2)
         //Get data and fill recyclerView
         runBlocking { getData(topWeekUrl) }
         recyclerView.adapter = adapter
-
-        val newGameButton = findViewById<Button>(R.id.new_game_button)
+        //New game button
         newGameButton.setOnClickListener {
-            //Chose game mode based on arraylength
-            //...
-            //random int between 0 and 1
-            //...
-            //Inport set number of questions
-            //Put questions into array
-            //Make Friendsgame for every question
-            //Add chosen movies to databse with Id of questions && foreinkey to Friendsgame
             if (RoomDBHelper.chosenMovieArr.size < 2 )
                 Toast.makeText(this,
                     "Choose at least twoo movies.", Toast.LENGTH_SHORT).show()
@@ -66,19 +81,29 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
-
-
-
+        //Search button
+        searchButton.setOnClickListener {
+            if(searchEditText.text.toString() == "")
+                Toast.makeText(this, "Chose a title first", Toast.LENGTH_SHORT).show()
+            else {
+                runBlocking {
+                    Log.e("urls", getMovieSearch(searchEditText.text.toString()))
+                    getData(getMovieSearch(searchEditText.text.toString()))
+                    adapter.notifyDataSetChanged()
+                }
+            }
+        }
     }
 
     suspend fun getData(topWeekUrl: String) = coroutineScope{
         //{"adult":false,"backdrop_path":"\/yYrvN5WFeGYjJnRzhY0QXuo4Isw.jpg","id":505642,"title":"Black Panther: Wakanda Forever","original_language":"en","original_title":"Black Panther: Wakanda Forever","overview":"Queen Ramonda, Shuri, M’Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers in the wake of King T’Challa’s death. As the Wakandans strive to embrace their next chapter, the heroes must band together with the help of War Dog Nakia and Everett Ross and forge a new path for the kingdom of Wakanda.","poster_path":"\/sv1xJUazXeYqALzczSZ3O6nkH75.jpg","media_type":"movie","genre_ids":[28,12,878],"popularity":4392.866,"release_date":"2022-11-09","video":false,"vote_average":7.6,"vote_count":630}
         val res = GetRequest().main(topWeekUrl)
         if (res.substring(0, 4) == "Some") {
-            Log.d("urlError", res.toString())
+            Log.d("urlError", res)
         } else {
             val jsonob = parse(res)
             if (jsonob != null) {
+                data.clear()
                 val movieRes = jsonob.getJSONArray("results")
                 for (i in 0 until movieRes.length()) {
                     data.add(movieRes.getJSONObject(i))
@@ -99,6 +124,9 @@ class MainActivity : AppCompatActivity() {
     fun getTrendingMoviesOfWeek(): String{
         return getBaseURL()+"trending/movie/day?"+getAPIKey()
     }
+    fun getMovieSearch(title: String): String{
+        return getBaseURL()+"search/movie?"+getAPIKey()+"&language=en-US&query="+title+"&page=1&include_adult=false"
+    }
 
     fun parse(json: String): JSONObject? {
         var jsonObject: JSONObject? = null
@@ -118,5 +146,26 @@ class MainActivity : AppCompatActivity() {
     fun startFriendsGame(){
         val intent = Intent(this, MovieBetweenFriends::class.java)
         startActivity(intent)
+    }
+
+    //Navigatie
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.custom_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.Search -> {
+//                Toast.makeText(this, "you", Toast.LENGTH_SHORT).show()
+                searchBar.visibility = View.VISIBLE
+                return true
+            }
+
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+
     }
 }
